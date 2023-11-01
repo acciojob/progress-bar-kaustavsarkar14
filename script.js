5a15fb965e05518bd4a9047517a8527d663a9164
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-11-01 18:25:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circle')
+let activeIndex = 0
+let activeCircle = circles[0]
+
+next.addEventListener('click', ()=>{ 
+	console.log(activeIndex, activeCircle)
+	activeIndex++
+	activeCircle = circles[activeIndex] 
+	if(activeCircle==null) return 
+	activeCircle.classList.add('active')
+})
+prev.addEventListener('click', ()=>{
+	if(activeCircle==null) return
+	activeCircle.classList.remove('active')
+	activeIndex--
+	activeCircle = circles[activeIndex] 
+	console.log(activeIndex, activeCircle)
+})
